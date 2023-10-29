a20bd7cd87cda87513a84cc834be103609dc0a0d
wasd_64 <wasd.rrre64@gmail.com>
2023-10-29 17:00:20 +0300

    add f/s name

--- bot/main.py
+++ bot/main.py
@@ -4,17 +4,25 @@ import os
 
 bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
 
+
 @bot.message_handler(commands=['start'])
 def start_func(message):
-    bot.send_message(message.chat.id,"Это бот МТУСИ кружка по спортивному программированию "
-                                     "для отправки кода на различные проверяющие системы")
+    bot.send_message(message.chat.id, "Это бот МТУСИ кружка по спортивному программированию "
+                                      "для отправки кода на различные проверяющие системы")
+    bot.send_message(message.chat.id, "Введите сначало имя, затем фамилию через пробел")
+    name = ""
+    surname = ""
+
+    @bot.message_handler(content_types=['text'])
+    def init_name(message):
+        name, surname = message.text.split()
+        bot.send_message(message.chat.id, f"Привет {surname} {name}")
 
 
 @bot.message_handler(commands=['help'])
 def help_func(message):
-    #TODO Help func
     pass
-    bot.send_message(message.chat.id,"")
+    bot.send_message(message.chat.id, "")
 
 
 if __name__ == "__main__":
