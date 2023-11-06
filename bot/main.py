fefb0dab92449bc09333669727de2bd8dfa5be10
wasd_64 <wasd.rrre64@gmail.com>
2023-11-07 00:15:10 +0300

    add help and user-info funcs

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,7 @@
 import telebot
 from sqlalchemy.orm import Session
-from telebot import types
+from sqlalchemy import select
+from telebot import types, util
 import os
 from BotDB import *
 
@@ -10,13 +11,13 @@ Base.metadata.create_all(db)
 
 
 class Docmaster:
-    _docs = None
+    __docs = None
 
     def __init__(self):
-        self._docs = []
+        self.__docs = []
 
     def docs(self, name, description, role):
-        self._docs.append({"name": name, "description": description, "role": role})
+        self.__docs.append({"name": name, "description": description, "role": role})
 
         def decorator(func):
             def wrapper(*args, **kwargs):
@@ -26,12 +27,31 @@ class Docmaster:
 
         return decorator
 
+    def print_user_funcs(self):
+        res = ""
+        for row in self.__docs:
+            if row["role"] == "user":
+                res += "<b>" + row["name"] + "</b>"
+                res += ' - '
+                res += row["description"]
+                res += '\n'
+        return res
 
-docs = Docmaster()
+    def print_admin_funcs(self):
+        res = ""
+        for row in self.__docs:
+            res += "<b>" + row["name"] + "</b>"
+            res += ' - '
+            res += row["description"]
+            res += '\n'
+        return res
 
 
-@docs.docs(name="/start", description="Starting Bot. If user telegram id is not in database, bot"
-                                      "requires name and secondname from user, delimiter = ' '", role='All')
+docums = Docmaster()
+
+
+@docums.docs(name="/start", description="Starting Bot. If user telegram id is not in database, bot "
+                                        "requires name and secondname from user, delimiter = ' '", role='user')
 @bot.message_handler(commands=['start'])
 def start_func(message):
     bot.send_message(message.chat.id, "Это бот МТУСИ кружка по спортивному программированию "
@@ -50,13 +70,28 @@ def __register(message):
             user = User(telegram_id=int(message.from_user.id), name=name, surname=sname)
             session.add(user)
             session.commit()
-        # db.insert_into_table(User, [{'telegram_id': int(message.from_user.id), 'name': name, 'surname': sname}])
     except ValueError:
         bot.send_message(message.chat.id, "Ошибка ввода!")
 
 
-# @bot.message_handler(commands=['help'])
-# def help_func(message):
+@bot.message_handler(commands=['get_user_info'])
+def get_user_info(message):
+    with Session(db) as session:
+        if session.query(User).filter_by(telegram_id=int(message.from_user.id)).first() is not None:
+            name, sname = [x for x in session.execute(select(User.name, User.surname)).first()]
+            bot.send_message(message.chat.id, f"Ваше фамилия и имя: {sname} {name}")
+
+
+@docums.docs(name="/help", description='Print description all func for your role', role='user')
+@bot.message_handler(commands=['help'])
+def help_func(message):
+    # Проверка на роль пользователя
+    if True:
+        for row in util.smart_split(docums.print_user_funcs(), 3000):
+            bot.send_message(message.chat.id, row, parse_mode='html')
+    # else:
+    #     for row in util.smart_split(docums.print_admin_funcs(), 3000):
+    #         bot.send_message(message.chat.id, row, parse_mode='html')
 
 
 if __name__ == "__main__":
