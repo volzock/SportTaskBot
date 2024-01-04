00db675502e91cf93be7ecb8cb63f420182f54ba
nkozlov <nukfeed@yandex.ru>
2024-01-04 17:53:54 +0300

    forgot to change token to env :)

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
 from routers import *
 
 
 def main():
-    bot = Bot(token="2005031664:AAFoT5J8xmjsNFF-npDPlrEjS2viViMRtqQ")
+    token = os.environ.get("BOT_TOKEN")
+
+    bot = Bot(token=token)
     dp = Dispatcher()
     dp.include_router(start_router)
 
